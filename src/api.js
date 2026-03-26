631de351c3e7128c60e449dc9f38711ec38f0ec8
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-03-26 12:05:54 +0100

    new

--- src/api.js
+++ src/api.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, doc, getDoc, query, where } from "firebase/firestore/lite"
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD3CNS5Az_VUxAvl2pjE7GdUJMFnteSjFE",
-  authDomain: "vanlife-9b805.firebaseapp.com",
-  projectId: "vanlife-9b805",
-  storageBucket: "vanlife-9b805.firebasestorage.app",
-  messagingSenderId: "703416533744",
-  appId: "1:703416533744:web:90faf442c41b2c66e7b835"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
